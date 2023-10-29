26ec56ba20cae8fde42ad378e004b41a01e789b5
shashank Jaipurkar <100088252+ShashankJaipurkar123@users.noreply.github.com>
2023-10-29 19:08:24 +0530

    Time: 37 ms (75.65%), Space: 6.9 MB (53.88%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int secondsToRemoveOccurrences(string s) {
+        int cnt=0;int st=0;int n=s.length();bool flag=false;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<n-1;j++){
+                if(s[j]=='0'&&s[j+1]=='1'){swap(s[j],s[j+1]);j++;flag=true;}
+            }
+            if(flag==false){return i;}flag=false;
+        }
+        return n;
+    }
+};
