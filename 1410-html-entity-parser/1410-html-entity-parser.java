a7e30cebf14d7b5266a51e686dd6dc0cfac8136b
shashank Jaipurkar <100088252+ShashankJaipurkar123@users.noreply.github.com>
2023-11-02 16:45:21 +0530

    Time: 21 ms (88.24%), Space: 44.3 MB (64.71%) - LeetHub

---
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String entityParser(String text) {
+        Map<String, Character> map = init_map();
+        StringBuilder str = new StringBuilder();
+        int i=0, j=0;
+        while(i<text.length()){
+            if(text.charAt(i)=='&'){
+                StringBuilder s = new StringBuilder();
+                j=i+1;
+                while(j<text.length() && text.charAt(j)!=';' && text.charAt(j)!='&') {
+                    s.append(text.charAt(j));
+                    j++;
+                }
+                if(j<text.length() && text.charAt(j)!='&'){
+                    char c = map.getOrDefault(s.toString(), '+');
+                    if(c!='+') {
+                        str.append(c);
+                        i=j+1;
+                        continue;
+                    }
+                }
+                str.append(text.charAt(i));
+                i++;
+            }else{
+                str.append(text.charAt(i));
+                i++;
+            }
+        }
+        return str.toString();
+    }
+
+    public Map<String, Character> init_map() {
+        Map<String, Character> map = new HashMap<String, Character>();
+        map.put("quot", '\"');
+        map.put("apos", '\'');
+        map.put("amp", '&');
+        map.put("gt", '>');
+        map.put("lt", '<');
+        map.put("frasl", '/');
+        return map;
+    }
+}
